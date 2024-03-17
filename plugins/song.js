031ce9bcdda98c7c7ee7f01f03265b053e7cb2be
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-17 20:48:25 +0530

    Create song.js

---
+++ plugins/song.js
@@ -0,0 +1,20 @@
+const { Ezra } = require("../lib/");
+const axios = require("axios");
+const fetch = require("node-fetch");
+
+Ezra(
+  {
+    pattern: "song",
+    desc: "YouTube song Downloader",
+    type: "downloader",
+  },
+  async ({client, msg, text}) => {
+if(!text)
+return msg.reply("_Enter a song name_");
+    let andi = await client.sendMessage(msg.chat, { text: "_Searching...!_" }, { quoted: msg });
+var res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${text}`)
+var song = res.data
+  await client.sendMessage(msg.chat, { text: `*_Downloading ${song.data.title}_*`, edit: andi.key }, { quoted: msg });  
+const axl = await (await fetch(`${song.data.downloadUrl}`)).buffer()
+await client.sendMessage(msg.chat , {audio : axl ,  mimetype : 'audio/mpeg'} , { quoted : msg })
+  });
