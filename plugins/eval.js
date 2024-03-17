c864a99d3befacc96c874f0dc5ebe9faafc3dd85
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-17 20:50:22 +0530

    Add files via upload

---
+++ plugins/eval.js
@@ -0,0 +1,24 @@
+/*const {
+    Ezra
+} = require("../index.js");
+const X = require("../config.js");
+const util = require("util");
+
+Ezra(
+    {
+        on: "text",
+        fromMe: true,
+    },
+    async ({
+        client, msg, text
+    }) => {
+        if (text.startsWith(">")) {
+            try {
+                let evaled = await eval(`(async () => { ${text.replace(">", "")} })()`);
+                if (typeof evaled !== "string") evaled = util.inspect(evaled);
+                await msg.reply(`${'```'}${evaled}${'```'}`)
+            } catch (err) {
+                await msg.reply(`_${util.format(err)}_`);
+            }
+        }
+    });*/
---
+++ plugins/ping.js
@@ -0,0 +1,12 @@
+const { Ezra } = require("../lib/");
+
+Ezra({
+    pattern: "ping",
+    desc: "To check ping",
+    type: "user",
+}, async ({ client, msg }) => {
+    const start = new Date().getTime();
+    let pong = await client.sendMessage(msg.chat, { text: "*ᴄʜᴇᴄᴋɪɴɢ ᴘɪɴɢ...*" }, { quoted: msg });
+    const end = new Date().getTime();
+    return await client.sendMessage(msg.chat, { text: `*Rᴇꜱᴘᴏɴꜱᴇ ɪɴ* *${end - start}* *ᴍꜱ*`, edit: pong.key }, { quoted: msg });
+});
