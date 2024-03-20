1f2f64cd09d7816f73b439b86bfce9e5cdf2da9d
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-20 16:40:17 +0530

    Create wave.js

---
+++ plugins/wave.js
@@ -0,0 +1,17 @@
+const { Ezra } = require("../lib/");
+
+Ezra(
+    {
+        pattern: "wave",
+        type: "converter",
+        desc: "audio in wave format"
+    },
+    async ({
+        msg, client, match
+    }) => {
+        if (!msg.quoted || !(msg.quoted.audioMessage || msg.quoted.documentMessage ))
+            return await msg.reply("_Reply to Audio Message_");
+        let buff = await msg.quoted.download()
+msg.sendMsg(msg.chat , buff,{ audiowave : Array.from({length: 30}, () => Math.floor(Math.random() * 100)) , ptt : true , mimetype : "audio/mpeg" } , "audio" )
+    }
+    );
