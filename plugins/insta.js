7c6272c78066c793f21d6f47a154bc7cbaaa945f
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-22 18:14:06 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -13,10 +13,10 @@ Ezra(
     }) => {
 if (!text) return await msg.reply("_Enter a insta Username_");
 //match = match || msg.quoted?.text;
-msg.reply("_Please wiat a moment!_\n_Searching..!_")
+msg.reply("_Searching..!_")
       
-let koothi = await fetch(`https://api-aswin-sparky.koyeb.app/api/search/ig?search=${text}`);
-var data = await koothi.json();
+let baby = await fetch(`https://api-aswin-sparky.koyeb.app/api/search/ig?search=${text}`);
+var data = await baby.json();
 ig = data.data
 
 client.sendMessage(msg.chat, { image: { url: ig.avatar }, caption:`*_ɴᴀᴍᴇ : ${ig.name}_*\n_*ᴜꜱᴇʀɴᴀᴍᴇ : ${ig.username}_*\n*_ꜰᴏʟʟᴏᴡᴇʀꜱ : ${ig.followers}_*\n*_ꜰᴏʟʟᴏᴡɪɴɢ : ${ig.following}_*\n*_ᴛᴏᴛᴀʟ ᴩᴏꜱᴛꜱ : ${ig.posts}_*\n*_ᴜꜱᴇʀ ʙɪᴏ_* : ${ig.description}` }, { quoted: msg });
