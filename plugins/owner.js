688b17003805c5d9cc77ae0fb7f026dab60f9f17
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-22 17:38:42 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,17 @@
+const {
+    Ezra
+} = require("../lib/");
+
+Ezra(
+    {
+        pattern: "mee",
+        fromMe: "true",
+        type: "owner",
+        desc: "self mention "
+    },
+    async ({
+        client, msg, text
+    }) => {
+msg.sendMsg(msg.chat , `_@${msg.sender.split("@")[0]}_`  , {   mentions : [msg.sender]} )
+  }
+);
