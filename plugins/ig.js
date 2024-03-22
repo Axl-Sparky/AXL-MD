8e777344dc477c7cc3bb6784a4a4b800a4d22f8c
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-22 16:58:42 +0530

    Update and rename insta.js to ig.js

---
+++ plugins/ig.js
@@ -0,0 +1,24 @@
+>const {
+    BOT_INFO
+} = require("../config.js");
+
+Ezra(
+    {
+        pattern: "ig",
+        type: "search",
+        desc: "search a insta id"
+    },
+    async ({
+        client, msg, text
+    }) => {
+if (!text) return await msg.reply("_Enter a insta Username_");
+//match = match || msg.quoted?.text;
+msg.reply("_Please wiat a moment!_\n_Searching..!_")
+      
+let koothi = await fetch(`https://api-aswin-sparky.koyeb.app/api/search/ig?search=${text}`);
+var data = await koothi.json();
+ig = data.data
+
+client.sendMessage(msg.chat, { image: { url: ig.avatar }, caption:`*_ɴᴀᴍᴇ :${ig.name}_*\n_*ᴜꜱᴇʀɴᴀᴍᴇ : ${ig.username}_*\n\n*_ꜰᴏʟʟᴏᴡᴇʀꜱ : ${ig.followers}_*\n*_ꜰᴏʟʟᴏᴡɪɴɢ : ${ig.following}_*\n*_ᴛᴏᴛᴀʟ ᴩᴏꜱᴛꜱ : ${ig.posts}_*\n*_ᴜꜱᴇʀ ʙɪᴏ_* : ${ig.description}` }, { quoted: msg });
+  }
+);
