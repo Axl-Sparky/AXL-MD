d0c1aaa7bcc9fae2ac82f03c49a965c5938f872d
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-23 12:12:56 +0530

    Update tool.js

--- plugins/tool.js
+++ plugins/tool.js
@@ -1,1 +1,23 @@
+const { remini } = require('../lib/remini.js')
 
+
+
+Axl(
+    {
+        pattern: "remini",
+        type: "converter",
+        fromMe: isPublic,
+        desc: "converte a photo HD quality"
+    },
+    async ({
+        client, msg, match
+    }) => {
+if (!msg.quoted || !(msg.quoted.imageMessage  )) return await msg.reply("_Replay to an photo/image_");
+await msg.reply("_Please wait..._");
+
+        let media = await msg.quoted.download()
+        let proses = await remini(media, "enhance")
+        await client.sendMessage(msg.chat, { image: proses }, { quoted: msg})
+
+  }
+);
