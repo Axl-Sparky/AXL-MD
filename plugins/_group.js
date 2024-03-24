dd5350b6d6e7b8c8950467a66d979f5507b2e9ac
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-24 19:28:59 +0530

    Update _group.js

--- plugins/_group.js
+++ plugins/_group.js
@@ -13,12 +13,12 @@ const X = require("../config.js");
     async ({
         msg, client, match
     }) => {
-let X = (msg.me);
+let AXL = (msg.me);
 let Y = (msg.sender);
 
 if (!await msg.isGroup) return msg.reply("_This is a group command!_")
 
-        if (!await msg.isAdmin(X)) return msg.reply("_I'm not an admin_")
+        if (!await msg.isAdmin(AXL)) return msg.reply("_I'm not an admin_")
 
         if (!await msg.isAdmin(Y)) return msg.reply("_This command for only group admins_")
         let code = await client.groupInviteCode(msg.chat)
