89da3dea248663053a237c1e0c1ec66975e5b0eb
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-25 14:16:31 +0530

    Add files via upload

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,12 +1,12 @@
-const { Axl } = require("../lib/");
+const { Ezra } = require("../lib/");
 
-Axl({
+Ezra({
     pattern: "ping",
     desc: "To check ping",
     type: "user",
 }, async ({ client, msg }) => {
     const start = new Date().getTime();
-    let pong = await client.sendMessage(msg.chat, { text: "_Checking Ping..._" }, { quoted: msg });
+    let pong = await client.sendMessage(msg.chat, { text: "*ᴄʜᴇᴄᴋɪɴɢ ᴘɪɴɢ...*" }, { quoted: msg });
     const end = new Date().getTime();
-    return await client.sendMessage(msg.chat, { text: "_Response in_ _${end - start}_ _Ms_", edit: pong.key }, { quoted: msg });
+    return await client.sendMessage(msg.chat, { text: `*Rᴇꜱᴘᴏɴꜱᴇ ɪɴ* *${end - start}* *ᴍꜱ*`, edit: pong.key }, { quoted: msg });
 });
