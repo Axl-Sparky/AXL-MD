6f29371df706b884e1149f6dcb18a57e5732fd82
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-25 17:14:29 +0530

    Update wave.js

--- plugins/wave.js
+++ plugins/wave.js
@@ -10,7 +10,7 @@ Axl(
         msg, client, match
     }) => {
         if (!msg.quoted || !(msg.quoted.audioMessage || msg.quoted.documentMessage ))
-            return await msg.reply("_Reply to Audio Message_");
+            return await msg.reply("_Reply to an Audio Message_");
         let buff = await msg.quoted.download()
 msg.sendMsg(msg.chat , buff,{ audiowave : Array.from({length: 30}, () => Math.floor(Math.random() * 100)) , ptt : true , mimetype : "audio/mpeg" } , "audio" )
     }
