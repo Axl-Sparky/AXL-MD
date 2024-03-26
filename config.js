7c5e94aca45636e7c266cfacb6b3000bad04575c
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-27 05:15:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     MODE: "public",
     SUDO: "919539412641",
     STICKER_DATA: process.env.STICKER_DATA || "AXL-MD;𖤍ᴀᴊꜱᴀʟ ꜱᴩᴀʀᴋy 𖤍",
-    BOT_INFO: process.env.BOT_INFO || "AXL-MD;AJSAL SPARKY;https://i.imgur.com/cmqBKpH.jpg",
+    BOT_INFO: process.env.BOT_INFO || "AXL-MD;AJSAL SPARKY;https://i.imgur.com/cmqBKpH.jpg;https://chat.whatsapp.com/HbekCMezwdr6bZXFjTNkLH",
     CAPTION: process.env.CAPTION || "_Speermy molu🥲❤️‍🩹!_",
 }
 
