c68c716b70c40a6043992a6a91210e809aab8065
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-27 12:49:22 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -11,10 +11,10 @@ Axl(
   async ({client, msg, text}) => {
 if(!text)
 return msg.reply("_Enter a song name!_\n_Eg:- play starboy_");
-    let andi = await client.sendMessage(msg.chat, { text: "_Please Wait_\n_Searching...!_" }, { quoted: msg });
+    let love = await client.sendMessage(msg.chat, { text: "_Please Wait_\n_Searching...!_" }, { quoted: msg });
 var res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${text}`)
 var song = res.data
-  await client.sendMessage(msg.chat, { text: `*_Downloading ${song.data.title}_*`, edit: andi.key }, { quoted: msg });  
+  await client.sendMessage(msg.chat, { text: `*_Downloading ${song.data.title}_*`, edit: love.key }, { quoted: msg });  
 const axl = await (await fetch(`${song.data.downloadUrl}`)).buffer()
 await client.sendMessage(msg.chat , {audio : axl ,  mimetype : 'audio/mpeg'} , { quoted : msg })
   });
