d3b09601dc4ac14f299b1cf15aaeb3125e58d3f0
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-30 11:49:37 +0530

    Create search.js

---
+++ plugins/search.js
@@ -0,0 +1,34 @@
+>const {
+    Axl
+} = require("../lib/");
+
+
+Axl(
+    {
+        pattern: "lyrics",
+        fromMe: true,
+        desc: "lyrics searcher",
+        type: "search",
+    },
+    async ({
+        msg, client, text
+    }) => {
+        if (!text)
+            return await msg.reply("_Enter a song name_");
+let dl = await client.sendMessage(msg.chat, {
+            text: "_searching..._"
+        }, {
+            quoted: msg
+        })
+try {
+let sample = await fetch(`https://api-aswin-sparky.koyeb.app/api/search/lyrics?search=${text}`);
+var data = await sample.json();
+let lyrics = data.data
+await msg.adreply(`${lyrics}`)
+} catch (e) {
+client.sendMessage(msg.chat, {
+                text: `_Error_`, edit: dl.key
+            })
+}
+
+});
