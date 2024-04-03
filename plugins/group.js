b94e2e552285e31ff4f66cec8f1b20b21d99899b
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-04-03 13:46:52 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -28,6 +28,74 @@ if (!await msg.isGroup) return msg.reply("_This is a group command!_")
     })
 
 
+Axl(
+    {
+        pattern: "mute",
+        fromMe: true,
+        desc: "Mutes the group.",
+        type: "group"
+    },
+    async ({
+        msg, client, match
+    }) => {
+
+        if (!await msg.isAdmin(msg.me)) return msg.reply("_I'm not an admin_")
+        if (!await msg.isAdmin(msg.sender)) return msg.reply("_This command for only group admins !_")
+        await client.groupSettingUpdate(msg.chat , 'announcement');
+        return await msg.reply("_Group Muted !_");
+    })
+
+
+Axl(
+    {
+        pattern: "unmute",
+        fromMe: true,
+        desc: "Mutes the group.",
+        type: "group"
+    },
+    async ({
+        msg, client, match
+    }) => {
+
+        if (!await msg.isAdmin(msg.me)) return msg.reply("_I'm not an admin_")
+        if (!await msg.isAdmin(msg.sender)) return msg.reply("_This command for only group admins !_")
+        await client.groupSettingUpdate(msg.chat , 'not_announcement');
+        return await msg.reply("_Group Unmuted !_");
+    })
+
+
+Axl(
+    {
+        pattern: "promote",
+        fromMe: true,
+        desc: "Promotes a user to admin",
+        type: "group"
+    },
+    async ({
+        msg, client, args, match, text }) => {
+        try {
+            if (!await msg.isAdmin(msg.me)) return msg.reply("_Admin access not conferred._")
+            if (!await msg.isAdmin(msg.sender)) return msg.reply("_Only for authorized administrators._")
+
+            if (!(match || msg.quoted)) return msg.reply("_Replay to a user!_")
+            if (match) {
+                var user = match.replace("@", "") + '@s.whatsapp.net';
+            } else if (msg.quoted.sender) {
+                var user = msg.quoted.sender;
+            } else if (msg.mentionedJid) {
+                var user = args + '@s.whatsapp.net';
+            }
+
+            /*if (await msg.isAdmin(user) === true) return msg.reply("_The user is currently in an admin role._");*/
+
+            await client.groupParticipantsUpdate(msg.chat, [user], "promote");
+            msg.sendMsg(msg.chat , `_@${user.split("@")[0]} promoted to admin!_`, { mentions : [user] , quoted : msg})
+        } catch (e) {
+            console.log(e);
+        }
+
+    })
+
 
 
 Axl(
@@ -51,7 +119,7 @@ Axl(
        }
             if (!await msg.isAdmin(msg.me)) return msg.reply("_I'm not an admin_")
             if (!await msg.isAdmin(msg.sender)) return msg.reply("_This command for only group admins_")
-            if (!(match || msg.quoted)) return msg.reply("_Mention a user._")
+            if (!(match || msg.quoted)) return msg.reply("_Replay to a user!_")
             if (match) {
                 var user = match.replace("@", "") + '@s.whatsapp.net';
             } else if (msg.quoted.sender) {
