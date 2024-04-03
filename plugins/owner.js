c560e90ac3ac66bb206c13b1cc9ef1831070f12e
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-04-03 12:32:41 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,6 +1,33 @@
+const { Axl } = require("../lib/");
+const util = require("util");
+const axios = require("axios");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
 const {
-    Axl
-} = require("../lib/");
+    exec
+} = require("child_process");
+
+
+
+Axl(
+    {
+        pattern: "restart",
+        fromMe: true,
+        desc: "Restart the bot",
+        type: "sudo",
+    },
+    async ( {
+        msg, args, client
+    }) => {
+        await msg.reply("_Restarting..._");
+        exec("node index.js", (error, stdout, stderr) => {
+            if (error) {
+                return client.sendMessage(msg.chat , `Error: ${error}`);
+            } return;
+        });
+    });
+
 
 Axl(
     {
