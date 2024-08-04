74e5d60f802d986fac12c6a1d42dad0fe734b798
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-08-04 16:14:39 +0530

    FIXED 🥂

--- plugins/blocking.js
+++ plugins/blocking.js
@@ -41,3 +41,44 @@ await client.updateBlockStatus(hho + "@s.whatsapp.net", "block")
    }
 
 });
+
+
+Axl(
+    {
+        pattern: "unblock",
+        fromMe: true,
+        desc: "unblock a user",
+        type: "owner",
+    },
+    async ({
+        msg, client, text, match
+    }) => {
+
+if (!text) return msg.reply("_Enter a whatsapp number !_");
+
+let guh = await client.sendMessage(msg.chat, {
+            text: "_Wait a sec !_"
+        }, {
+            quoted: msg
+        })
+
+try {
+
+let oih = `${text}`
+    
+await client.updateBlockStatus(oih + "@s.whatsapp.net", "block")
+
+  await client.sendMessage(msg.chat, {
+                text: `_User Unblocked_`, edit: dl.key
+                  
+//await msg.reply("_User Blocked_")
+  
+
+} catch (e) {
+            client.sendMessage(msg.chat, {
+                text: `_Error_`, edit: guh.key
+            })
+
+   }
+
+});
