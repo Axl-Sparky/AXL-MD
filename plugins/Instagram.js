889402f63c97fbbef2fa43eb48933cae860f887c
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-08-07 17:41:17 +0530

    Create Instagram.js

---
+++ plugins/Instagram.js
@@ -0,0 +1,36 @@
+const { Axl, getJson } = require("../lib/");
+const config = require("../config");
+const { CAPTION } =  require("../config");
+
+
+Axl(
+    {
+        pattern: "insta",
+    desc: "no wojjrds",
+    type: "user",
+    },
+    async ({
+        msg, client, text
+    }) => {
+
+if(!text) return msg.reply("_Enter a insta Url_")
+
+
+      let { result } = await getJson(`https://afiya-web-api.onrender.com/api/insta?url=${text}`)
+
+   
+  await client.sendMessage(msg.chat, {
+            text: "_Wait a sec !_"
+        }, {
+            quoted: msg
+        })
+
+      for (let i = 0; i < result.length; i++) {
+      await msg.sendFromUrl(result[i], {quoted: msg,
+caption: (config.CAPTION)});
+}
+
+
+
+
+    })
