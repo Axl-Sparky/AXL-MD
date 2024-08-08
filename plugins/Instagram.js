b006f5fb99776472424a0c683f89e028fd00c822
Barish <88338865+Viper-x0@users.noreply.github.com>
2024-08-08 20:44:42 +0530

    Update Instagram.js

--- plugins/Instagram.js
+++ plugins/Instagram.js
@@ -6,8 +6,8 @@ const { CAPTION } =  require("../config");
 Axl(
     {
         pattern: "insta",
-    desc: "no wojjrds",
-    type: "user",
+        desc: "no wojjrds",
+        type: "user",
     },
     async ({
         msg, client, text
@@ -15,10 +15,8 @@ Axl(
 
 if(!text) return msg.reply("_Enter a insta Url_")
 
-
       let { result } = await getJson(`https://afiya-web-api.onrender.com/api/insta?url=${text}`)
 
-   
   await client.sendMessage(msg.chat, {
             text: "_Wait a sec !_"
         }, {
@@ -29,8 +27,6 @@ if(!text) return msg.reply("_Enter a insta Url_")
       await msg.sendFromUrl(result[i], {quoted: msg,
 caption: (config.CAPTION)});
 }
-
-
 
 
     })
