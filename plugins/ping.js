c464c1e579fd1b79b8de67955e18a6178fd49438
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-05 19:40:12 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -6,7 +6,7 @@ Axl({
     type: "user",
 }, async ({ client, msg }) => {
     const start = new Date().getTime();
-    let pong = await client.sendMessage(msg.chat, { text: "_Checking..._" }, { quoted: msg });
+    let pong = await client.sendMessage(msg.chat, { text: "_Checking...!_" }, { quoted: msg });
     const end = new Date().getTime();
     return await client.sendMessage(msg.chat, { text: `_Response in_ _${end - start}_ _Ms_`, edit: pong.key }, { quoted: msg });
 });
